f1e6ac55867d218fda750c0475dd3a393c7dbeb0
Christopher Kim <chrisms91@gmail.com>
2025-01-27 12:57:08 -0800

    added like count column on the tweet admin panel using related_name

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from .models import Tweet, Like
 
+
 # Register your models here.
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = ("user", "payload", "total_likes")
+
+    def total_likes(self, tweet):
+        return tweet.likes.all().count()
 
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
     pass
-
--- tweets/models.py
+++ tweets/models.py
@@ -12,7 +12,10 @@ class Tweet(CommonModel):
     """
 
     payload = models.TextField(max_length=180)
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.payload
@@ -27,7 +30,11 @@ class Like(CommonModel):
     """
 
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    tweet = models.ForeignKey("tweets.Tweet", on_delete=models.CASCADE)
+    tweet = models.ForeignKey(
+        "tweets.Tweet",
+        on_delete=models.CASCADE,
+        related_name="likes",
+    )
 
     def __str__(self):
         return f"{self.tweet.payload}"
